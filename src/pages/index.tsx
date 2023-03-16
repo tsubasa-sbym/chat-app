06cb9cec64cd97841812c8d120a4b5f2340477bb
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-03-16 23:35:45 +0900

    delete: 不要要素の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
@@ -5,7 +6,7 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const Page: NextPage = () => {
   return (
     <>
       <Head>
@@ -121,3 +122,5 @@ export default function Home() {
     </>
   )
 }
+
+export default Page
