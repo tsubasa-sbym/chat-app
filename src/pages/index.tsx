e793d1d12bd53332f7406b7f3961c8fe211ecbf8
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-03-18 20:44:04 +0900

    feat: 不要箇所コメントアウト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const Page: NextPage = () => {
   return (
