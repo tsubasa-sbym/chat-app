64959be40e17853ecf8bf334bae310dd0b4a1a38
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-03-19 22:12:35 +0900

    feat: firebaseConfigにlocallで設定した環境変数を適用＆firebaseの初期化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
+// import { getApp } from 'firebase/app'
 
+initializeFirebaseApp()
+// console.log(getApp())
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <ChakraProvider>
