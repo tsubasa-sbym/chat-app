25b8d1cc7ae71f879d64d0939fcb9a9eff3ed87b
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-03-19 22:53:23 +0900

    feat: フォームの入力値の状態管理

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -11,6 +11,7 @@ import {
   Input,
   Spacer,
 } from '@chakra-ui/react'
+import { FormEvent, useState } from 'react'
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 import { FirebaseError } from '@firebase/util'
 
@@ -26,20 +27,42 @@ import { FirebaseError } from '@firebase/util'
 // }
 
 export const Page = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    console.log({ email, password })
+    e.preventDefault()
+  }
+
   return (
     <Container py={14}>
       <Heading>サインアップ</Heading>
-      <chakra.form>
+      <chakra.form onSubmit={handleSubmit}>
         <Spacer height={8} aria-hidden />
         <Grid gap={4}>
           <Box display={'contents'}>
             <FormControl>
               <FormLabel>メールアドレス</FormLabel>
-              <Input type={'email'} />
+              <Input
+                type={'email'}
+                name={'email'}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>パスワード</FormLabel>
-              <Input type={'password'} />
+              <Input
+                type={'password'}
+                name={'password'}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+              />
             </FormControl>
           </Box>
         </Grid>
