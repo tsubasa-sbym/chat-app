7fe6be1b768717bee43b568efcd20bdd921a4e78
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-03-21 21:15:53 +0900

    feat: Headerコンポーネントの作成とapp.tsxでの読み込み

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
 import { AuthProvider } from '@src/feature/auth/provider/AuthProvider'
+import { Header } from '@src/component/Header/Header'
 
 initializeFirebaseApp()
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <ChakraProvider>
         <AuthProvider>
+          <Header />
           <Component {...pageProps} />
         </AuthProvider>
       </ChakraProvider>
